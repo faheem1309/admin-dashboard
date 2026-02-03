06bf9e0199ff7e810f81e7792e62ade3c637df4f
faheem1309 <masfmohammed027@gmail.com>
2026-02-03 11:43:27 +0530

    Create UsersTable.jsx

---
+++ src/components/UsersTable.jsx
@@ -0,0 +1,22 @@
+export default function UsersTable({ users }) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
