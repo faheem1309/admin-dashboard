6b6702e849d1b79a30d09c460de690f8ce08fe39
faheem1309 <masfmohammed027@gmail.com>
2026-02-03 11:51:12 +0530

    Create mockData.js

---
+++ src/data/mockData.js
@@ -0,0 +1,10 @@
+export const stats = [
+{ title: "Users", value: 1200 },
+{ title: "Revenue", value: "$45,000" },
+{ title: "Orders", value: 320 }
+];
+
+export const users = [
+{ id: 1, name: "Ali", email: "ali@mail.com" },
+{ id: 2, name: "Sara", email: "sara@mail.com" }
+];
